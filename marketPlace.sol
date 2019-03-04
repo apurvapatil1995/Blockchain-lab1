f5ace6ea0f837bfadf4cd7b40ee2b3c377308704
Xynoclafe <gokulprem.94@gmail.com>
2019-03-03 22:35:58 -0500

    smart contract modification, db modification

--- marketPlace.sol
+++ marketPlace.sol
@@ -9,21 +9,48 @@ contract marketPlace{
     
     struct customer {
         uint itemId;
+	    bool registered;
     }
     
+    modifier onlyChairperson() {
+        require(msg.sender == chairman);
+        _;
+    }
     mapping(address => customer) customers;
     
     event listed(uint itemId);
     event sold(uint itemId);
     
     function sell(uint itemId) public{
+        if(customers[msg.sender].registered == false) revert();
         customer storage seller = customers[msg.sender];
         seller.itemId = itemId;
         emit listed(itemId);
+    }
+    
+    function register(address newUser) public onlyChairperson{
+        if(customers[newUser].registered == true) revert();
+            customers[newUser].registered == true;
+    }
+    
+    function unregister(address user) public onlyChairperson{
+        if(customers[user].registered == false) revert();
+        customers[user].registered == false;
+    }
+    
+    function settlePayment() private{
         
     }
     
-    function buy(uint itemId, address seller) public{
+    function addBalance() private{
+        
+    }
+    
+    function buy(uint itemId, int itemPrice, address seller, int balance) public{
+        if(customers[msg.sender].registered == false) revert();
+        if(customers[seller].registered == false) revert();
+        if(itemPrice > balance) revert();
+        settlePayment();
         customer storage buyer = customers[msg.sender];
         customer storage seller = customers[seller];
         buyer.itemId = itemId;
